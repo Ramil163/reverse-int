f9411ed8d76d560e7a66aba022b3b772bc23f7b8
Ramil <mustafin.rf@yandex.ru>
2021-08-17 16:53:17 +0400

    feat: added inner to func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let arr = String(n);
+    let arr2 = arr.split(" ");
+    let arrRev = arr2.reverse();
+    let strRev = arrRev.join();
+    let ans = Number(strRev);
+    return ans;
+};
